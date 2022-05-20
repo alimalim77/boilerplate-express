f6d15ba0e8e52fbaad6419f5eba30910e9fbf753
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-05-20 17:45:48 +0530

    Backend Changes

--- myApp.js
+++ myApp.js
@@ -1,13 +1,20 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+console.log('Hello World');
 
 app.use('/public',express.static(__dirname + '/public'));
-app.get("/",
-    function(req,res){
-        res.sendFile(__dirname + "/views/index.html");
-    }
-)
+
+app.get('/json',function (req,res){
+  const data = {
+    'message': 'Hello json'
+  };
+  res.json(data);
+})
+
+app.get('/',function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
 
 
 
